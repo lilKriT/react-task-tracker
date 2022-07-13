1940301a50dff3925b6d715064fe35244770f8dd
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-13 19:58:45 +0200

    Adding tasks works.

--- src/App.js
+++ src/App.js
@@ -25,6 +25,17 @@ function App() {
     },
   ]);
 
+  // add Task
+  const addTask = (task) => {
+    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = {
+      id,
+      ...task,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
   // delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -43,7 +54,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
